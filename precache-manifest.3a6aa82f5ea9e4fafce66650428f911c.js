f66fa9e5172474903edec79d827d0ec824f91194
Anastasia <nastiyayesypenko@ukr.net>
2019-06-29 21:38:35 +0300

    Updates

---
+++ precache-manifest.3a6aa82f5ea9e4fafce66650428f911c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad55bd17c504d56523026b6b2812374",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "f0c2fa352ef58a506f9f",
+    "url": "/goit-react-hw-04-reader/static/css/reader-page.81300a38.chunk.css"
+  },
+  {
+    "revision": "1112ddcdeac0b23a673e",
+    "url": "/goit-react-hw-04-reader/static/js/3.dbf52f03.chunk.js"
+  },
+  {
+    "revision": "c44b5879497e7022ac49",
+    "url": "/goit-react-hw-04-reader/static/js/main.f56cd37c.chunk.js"
+  },
+  {
+    "revision": "f0c2fa352ef58a506f9f",
+    "url": "/goit-react-hw-04-reader/static/js/reader-page.51ddf284.chunk.js"
+  },
+  {
+    "revision": "5fd50fdc9785a0cc68b2",
+    "url": "/goit-react-hw-04-reader/static/js/runtime~main.f430616a.js"
+  }
+]);
